7f4389717ade5c2d110d3ec5b6467e832c679cfb
Muhammad Yasin Shereef <muhdyasin805@gmail.com>
2026-03-01 07:00:46 +0530

    Wire evaluator and explanation service in Program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DecisionCompanion.Domain;
+using DecisionCompanion.Application;
+
+var criteria = new List<Criterion>
+{
+    new Criterion("Performance", 9),
+    new Criterion("Battery", 1)
+};
+
+var options = new List<Option>
+{
+    new Option("Laptop A", new Dictionary<string, double>
+    {
+        { "Performance", 8 },
+        { "Battery", 10 }
+    }),
+    new Option("Laptop B", new Dictionary<string, double>
+    {
+        { "Performance", 9 },
+        { "Battery", 1 }
+    })
+};
+
+IDecisionEvaluator evaluator = new WeightedDecisionEvaluator();
+var rankedResults = evaluator.Evaluate(options, criteria);
+
+var explanationService = new ExplanationService();
+var explanation = explanationService.GenerateExplanation(rankedResults);
+
+Console.WriteLine(explanation);
